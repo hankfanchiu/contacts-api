6ac80ccc74ec3757495270a675f61056e270d29d
App Academy Student <student@appacademy.io>
2015-11-16 15:14:39 -0800

    Test UsersController methods with script

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -1,24 +1,48 @@
 require 'addressable/uri'
 require 'rest-client'
 
-url = Addressable::URI.new(
+# users = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users.html'
+# ).to_s
+#
+# puts RestClient.get(users)
+#
+# user_1 = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users/1'
+# ).to_s
+#
+# puts RestClient.get(user_1)
+
+# new_user = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users'
+# ).to_s
+# params = {:user => {:username => 'batman@test.com'}}
+# puts RestClient.post(new_user, params)
+
+# update_user = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users/1'
+# ).to_s
+#
+# params = {:user => {:username => 'therealzac@test.com'}}
+# puts RestClient.put(update_user, params)
+
+delete_user = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users.html'
+  path: '/users/3'
 ).to_s
 
-puts RestClient.get(url)
-
-url2 = Addressable::URI.new(
-  scheme: 'http',
-  host: 'localhost',
-  port: 3000,
-  path: '/users/5.json',
-  query_values: {
-    'category[key]' => 'value2',
-    'another_category[key2]' => 'sup'
-  }
-).to_s
-
-puts RestClient.get(url2)
+puts RestClient.delete(delete_user)
